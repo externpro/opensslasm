9a56217d91d57ac932b684b91bc7d486a08ba7c7
Scott M Anderson <smanders@sdl.usu.edu>
2019-12-09 17:13:20 -0700

    initial copyfromnode.cmake script
    execute: `cmake -P copyfromnode.cmake`

---
+++ copyfromnode.cmake
@@ -0,0 +1,17 @@
+# cmake -P copyfromnode.cmake
+set(NODE_VER 10.17.0)
+set(NODE_DIR ${CMAKE_CURRENT_LIST_DIR}/../node_${NODE_VER}_repo/deps/openssl/config)
+if(NOT EXISTS ${NODE_DIR})
+  message(FATAL_ERROR "directory ${NODE_DIR} does not exist: update NODE_VER (${NODE_VER})?")
+endif()
+foreach(d
+  archs/linux-x86_64/*.s
+  archs/VC-WIN64A/*.asm
+  )
+  file(GLOB_RECURSE files RELATIVE ${NODE_DIR} ${NODE_DIR}/${d})
+  foreach(f ${files})
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${NODE_DIR}/${f} ${f}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+      )
+  endforeach()
+endforeach()
