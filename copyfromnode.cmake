6bff63151916fe9869f0b8fedd4dfa6b6bba23e3
Scott M Anderson <smanders@sdl.usu.edu>
2021-05-28 22:05:20 +0000

    openssl asm from node 14.17.0

--- copyfromnode.cmake
+++ copyfromnode.cmake
@@ -1,6 +1,6 @@
 # cmake -P copyfromnode.cmake
-set(NODE_VER 10.17.0)
-set(NODE_DIR ${CMAKE_CURRENT_LIST_DIR}/../node_${NODE_VER}_repo/deps/openssl/config)
+set(NODE_VER 14.17.0)
+set(NODE_DIR ${CMAKE_CURRENT_LIST_DIR}/../nodejs_${NODE_VER}_repo/deps/openssl/config)
 if(NOT EXISTS ${NODE_DIR})
   message(FATAL_ERROR "directory ${NODE_DIR} does not exist: update NODE_VER (${NODE_VER})?")
 endif()
