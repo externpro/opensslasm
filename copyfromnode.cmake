ba87ac6b89350f406074c70f7381fe102f30bc50
Scott M Anderson <anderson.scott.m@icloud.com>
2025-11-11 11:09:20 -0700

    copyfromnode updated, match openssl version

--- copyfromnode.cmake
+++ copyfromnode.cmake
@@ -1,16 +1,46 @@
 # cmake -P copyfromnode.cmake
-set(NODE_VER 14.17.6)
-set(NODE_DIR ${CMAKE_CURRENT_LIST_DIR}/../nodejs_${NODE_VER}_repo/deps/openssl/config)
-if(NOT EXISTS ${NODE_DIR})
-  message(FATAL_ERROR "directory ${NODE_DIR} does not exist: update NODE_VER (${NODE_VER})?")
+# Function to extract OpenSSL version from opensslv.h
+function(extractOpensslVersion opensslv_h version_var)
+  file(STRINGS ${opensslv_h} version_line
+   REGEX "^#[ ]*define[ ]+OPENSSL_VERSION_TEXT"
+   LIMIT_COUNT 1
+   )
+  if(version_line MATCHES "OPENSSL_VERSION_TEXT[ \t]+\"([^\"]+)\"")
+    set(${version_var} ${CMAKE_MATCH_1} PARENT_SCOPE)
+  else()
+    message(FATAL_ERROR "Failed to extract OpenSSL version from ${opensslv_h}")
+  endif()
+endfunction()
+set(opensslRootDir ${CMAKE_CURRENT_LIST_DIR}/..)
+set(opensslVerFile include/openssl/opensslv.h)
+set(mainOpensslHeader ${opensslRootDir}/${opensslVerFile})
+set(nodeOpensslDir ${opensslRootDir}/../node/deps/openssl)
+set(nodeOpensslHeader ${nodeOpensslDir}/openssl/${opensslVerFile})
+set(nodeAsmDir ${nodeOpensslDir}/config)
+if(NOT EXISTS ${nodeOpensslHeader})
+  message(FATAL_ERROR "Node.js OpenSSL header not found at: ${nodeOpensslHeader}")
+endif()
+extractOpensslVersion(${mainOpensslHeader} mainOpensslVersion)
+extractOpensslVersion(${nodeOpensslHeader} nodeOpensslVersion)
+if(NOT "${mainOpensslVersion}" STREQUAL "${nodeOpensslVersion}")
+  message(FATAL_ERROR
+    "OpenSSL version mismatch!\n"
+    "Main OpenSSL version: ${mainOpensslVersion}\n"
+    "Node.js OpenSSL version: ${nodeOpensslVersion}\n"
+    "Versions must match to ensure compatibility.")
+else()
+  message(STATUS "OpenSSL versions match: ${mainOpensslVersion}")
+endif()
+if(NOT EXISTS ${nodeAsmDir})
+  message(FATAL_ERROR "directory ${nodeAsmDir} does not exist: update node repo?")
 endif()
 foreach(d
   archs/linux-x86_64/*.s
   archs/VC-WIN64A/*.asm
   )
-  file(GLOB_RECURSE files RELATIVE ${NODE_DIR} ${NODE_DIR}/${d})
+  file(GLOB_RECURSE files RELATIVE ${nodeAsmDir} ${nodeAsmDir}/${d})
   foreach(f ${files})
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${NODE_DIR}/${f} ${f}
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${nodeAsmDir}/${f} ${f}
       WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
       )
   endforeach()
